f84d3056bbfa651db135d333382341ede0fb9449
Thalys Row <thalysrogue@gmail.com>
2023-12-29 16:52:41 -0300

    add types

---
+++ src/types/types.ts
@@ -0,0 +1,28 @@
+import { StringSchema } from "joi";
+
+export type User = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type Categories = {
+  id: number;
+  description: string;
+};
+
+export type Transactions = {
+  id: number;
+  description: string;
+  value: number;
+  date: Date;
+  category_id: number;
+  user_id: number;
+  type: string;
+};
+
+export type SchemaUser = {
+  name: StringSchema<string>;
+  email: StringSchema<string>;
+  password: StringSchema<string>;
+};
