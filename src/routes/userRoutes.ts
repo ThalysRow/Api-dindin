9f3385eb2440d177dc527b2d65ddb33ceeb8031d
Thalys Row <thalysrogue@gmail.com>
2023-12-31 17:59:27 -0300

    add route login

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { newUser } from "../controllers/userControllers";
+import { loginUser, newUser } from "../controllers/userControllers";
 import { validateNewUser } from "../middleware/userValidate";
 import { validateBody } from "../middleware/validateBody";
 import { schemaUser } from "../utils/schemas";
+import { validateLogin } from "../middleware/validateLogin";
 
 const userRouter = Router();
 
 userRouter.post("/user", validateBody(schemaUser), validateNewUser, newUser);
+userRouter.post("/login", validateLogin, loginUser);
 
 export default userRouter;
