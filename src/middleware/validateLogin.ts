3ab72ead2fd817ae2d0ba89ac60c67c05f6287a3
Thalys Row <thalysrogue@gmail.com>
2023-12-31 18:00:32 -0300

    add validate login

---
+++ src/middleware/validateLogin.ts
@@ -0,0 +1,34 @@
+import { knex } from "../database/connection";
+import { Request, Response, NextFunction } from "express";
+import { User } from "../types/types";
+import bcrypt from "bcrypt";
+
+export const validateLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "email and password are required" });
+  }
+
+  try {
+    const emailFind = await knex<User>("users").where("email", email).first();
+    if (!emailFind) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const pass = emailFind.password;
+
+    const validatePass = await bcrypt.compare(password, pass);
+    if (!validatePass) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(400).json({ message: "Erro in validate login" });
+  }
+};
