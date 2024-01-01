f5ed933f555c77af678c58e5d92a02ed23dd3358
Thalys Row <thalysrogue@gmail.com>
2023-12-31 22:14:35 -0300

    add validate update user

---
+++ src/middleware/validateUpdateUser.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import { knex } from "../database/connection";
+import bcrypt from "bcrypt";
+import { findUserByEmail } from "../utils/usersFunctions";
+interface CustomRequest extends Request {
+  userId?: number;
+}
+
+export const validateUpdateUser = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email } = req.body;
+  try {
+    const user = await findUserByEmail(email);
+    if (user && user.id !== req.userId) {
+      return res.status(400).json({ message: "email in use" });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(400).json({ message: "Erro in validate update user" });
+  }
+};
