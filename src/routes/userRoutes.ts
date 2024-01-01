f3dc8fce591afba29ad31c615ec307e4ac230881
Thalys Row <thalysrogue@gmail.com>
2023-12-31 22:16:06 -0300

    add route update user

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { loginUser, newUser, userInfo } from "../controllers/userControllers";
-import { validateNewUser } from "../middleware/userValidate";
+import {
+  loginUser,
+  newUser,
+  updateUser,
+  userInfo,
+} from "../controllers/userControllers";
+import { validateNewUser } from "../middleware/validateNewUser";
 import { validateBody } from "../middleware/validateBody";
 import { schemaUser } from "../utils/schemas";
 import { validateLogin } from "../middleware/validateLogin";
 import { authentication } from "../middleware/authentication";
+import { validateUpdateUser } from "../middleware/validateUpdateUser";
 
 const userRouter = Router();
 
@@ -14,5 +20,11 @@ userRouter.post("/login", validateLogin, loginUser);
 userRouter.use(authentication);
 
 userRouter.get("/user", userInfo);
+userRouter.put(
+  "/user",
+  validateBody(schemaUser),
+  validateUpdateUser,
+  updateUser
+);
 
 export default userRouter;
