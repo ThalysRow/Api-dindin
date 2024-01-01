68e85dbc11b2a9bf851a4a2434f223731be8adeb
Thalys Row <thalysrogue@gmail.com>
2023-12-31 22:13:41 -0300

    add validate new user

---
+++ src/middleware/validateNewUser.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import { knex } from "../database/connection";
+import { User } from "../types/types";
+
+export const validateNewUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email } = req.body;
+  try {
+    const user = await knex<User>("users").where("email", email).first();
+    if (user) {
+      return res.status(400).json({ message: "E-mail already registered" });
+    }
+    next();
+  } catch (error) {
+    return res.status(400).json({ message: "Erro in validate new user" });
+  }
+};
