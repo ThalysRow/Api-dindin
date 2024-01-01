844f0c2f5c2fabe0c22c6777bf884a8ba3545bc1
Thalys Row <thalysrogue@gmail.com>
2023-12-31 22:41:15 -0300

    add route category

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import userRouter from "./routes/userRoutes";
+import categoryRouter from "./routes/categoryRoutes";
 
 const app = express();
 
 app.use(express.json());
 app.use(userRouter);
+app.use(categoryRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("Running");
