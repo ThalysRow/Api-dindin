70576a88b24e974f666a13a5872042be6dd2ecc7
Thalys Row <thalysrogue@gmail.com>
2024-01-02 18:37:37 -0300

    add validate get transaction

---
+++ src/middleware/validateGetTransaction.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import { knex } from "../database/connection";
+interface CustomRequest extends Request {
+  userId?: number;
+}
+
+export const validateGetTransaction = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  try {
+    const transaction = await knex("transactions").where("id", id).first();
+
+    if (!transaction || transaction.user_id !== req.userId) {
+      return res.status(404).json({ message: "Transaction not found" });
+    }
+
+    next();
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ message: "Erro in validate get transaction" });
+  }
+};
