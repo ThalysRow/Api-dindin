090e165b19e72ef5a0243c309ae178661b06634a
Thalys Row <thalysrogue@gmail.com>
2024-01-02 18:13:03 -0300

    add route listen transactions

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -2,17 +2,22 @@ import { Router } from "express";
 import { validateBody } from "../middleware/validateBody";
 import { schemaTransaction } from "../utils/schemas";
 import { validateNewTransaction } from "../middleware/validateNewTransaction";
-import { newTransation } from "../controllers/transactionsControllers";
+import {
+  newTransation,
+  transactionsListen,
+} from "../controllers/transactionsControllers";
 import { authentication } from "../middleware/authentication";
 
 const transactionRouter = Router();
 
 transactionRouter.use(authentication);
+
 transactionRouter.post(
   "/transaction",
   validateBody(schemaTransaction),
   validateNewTransaction,
   newTransation
 );
+transactionRouter.get("/transaction", transactionsListen);
 
 export default transactionRouter;
