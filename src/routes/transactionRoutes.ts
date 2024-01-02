b781a601ca03e2b06c4823c3ffb521998a7d76c6
Thalys Row <thalysrogue@gmail.com>
2024-01-02 20:12:15 -0300

    add update transaction route

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { validateBody } from "../middleware/validateBody";
 import { schemaTransaction } from "../utils/schemas";
-import { validateNewTransaction } from "../middleware/validateNewTransaction";
+import { validateNewTransaction } from "../middleware/transactions/validateNewTransaction";
 import {
   getTransaction,
   newTransation,
   transactionsListen,
+  updateTransaction,
 } from "../controllers/transactionsControllers";
 import { authentication } from "../middleware/authentication";
-import { validateGetTransaction } from "../middleware/validateGetTransaction";
+import { validateGetTransaction } from "../middleware/transactions/validateGetTransaction";
+import { validateUpdateTransaction } from "../middleware/transactions/validateUpdateTransaction";
 
 const transactionRouter = Router();
 
@@ -20,11 +22,20 @@ transactionRouter.post(
   validateNewTransaction,
   newTransation
 );
+
 transactionRouter.get("/transaction", transactionsListen);
+
 transactionRouter.get(
   "/transaction/:id",
   validateGetTransaction,
   getTransaction
+);
+
+transactionRouter.put(
+  "/transaction/:id",
+  validateBody(schemaTransaction),
+  validateUpdateTransaction,
+  updateTransaction
 );
 
 export default transactionRouter;
