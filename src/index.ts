ed04b06e337c1cc6fa7ecef7ffd62927d293018b
Thalys Row <thalysrogue@gmail.com>
2024-01-01 23:06:11 -0300

    add route transaction

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from "express";
 import "dotenv/config";
 import userRouter from "./routes/userRoutes";
 import categoryRouter from "./routes/categoryRoutes";
+import transactionRouter from "./routes/transactionRoutes";
 
 const app = express();
 
 app.use(express.json());
 app.use(userRouter);
 app.use(categoryRouter);
+app.use(transactionRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("Running");
